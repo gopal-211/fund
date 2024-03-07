944d6e39995776191297888202a325cc4a2123f8
gopal-211 <learncode973@gmail.com>
2024-03-07 07:20:43 +0530

    vite rollup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@safe-globalThis/safe-ethers-adapters', '@safe-globalThis/safe-core-sdk','@safe-globalThis/safe-ethers-lib']
+    }
+  },
   define: {
     global: "globalThis",
     "process.env": {},
